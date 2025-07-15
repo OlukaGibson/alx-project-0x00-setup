3bdaa2e9ab182e87b6bc3cea7f2c8d13d8d6582a
OlukaGibson <gibsonoluka7@gmail.com>
2025-07-15 20:28:11 +0300

    done

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -7,15 +7,16 @@ const Landing: React.FC = () => {
       <h1 className="text-xl font-extralight mb-4">Landing Page</h1>
       <Card />
       <Card />
-      <div className="flex gap-2 mt-4">
+      {/* <div className="flex gap-2 mt-4">
         <Button title="Small" size="small" rounded="sm" />
         <Button title="Medium" size="medium" rounded="md" />
         <Button title="Large" size="large" rounded="full" />
-      </div>
+      </div> */}
       <div className="hidden">
   <button className="rounded-sm"></button>
   <button className="rounded-md"></button>
   <button className="rounded-full"></button>
+  <button className="rounded-lg"></button>
 </div> 
     </div>
   );
